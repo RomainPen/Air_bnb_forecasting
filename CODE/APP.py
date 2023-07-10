6903c075aa5502ada70272282caaf2534ff40d4b
RomainPen <romain.pen.pro16@gmail.com>
2023-07-10 20:34:18 +0200

    update app.py

--- CODE/APP.py
+++ CODE/APP.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image
+from datetime import datetime, timedelta
 
 import pandas as pd
 import pickle
@@ -38,37 +39,45 @@ def main():
     # Title :
     st.title('What is the rental price for a house in Marseille ? :house:')
 
-    # Features params :
-    traveler = st.slider("number of traveler", 1, 15) 
-    bathroom = st.slider("number of bathroom", 0, 10)
-    free_parking_on_site = st.select_slider("Free parking on site", [0,1])
-    free_street_parking = st.select_slider("Free parking on street", [0,1])
-    heating = st.select_slider("heating", [0,1])
-    seaview = st.select_slider("seaview", [0,1])
-    AC = st.select_slider("AC (climatisation)", [0,1])
-    wifi = st.select_slider("wifi", [0,1])
-    accepted_animals = st.select_slider("accepted animals", [0,1])
-    tv = st.select_slider("tv", [0,1])
-    microwave_oven = st.select_slider("microwave_oven", [0,1])
-    smoker = st.select_slider("smoker", [0,1])
-    backyard = st.select_slider("backyard", [0,1])
-    workspace = st.select_slider("workspace", [0,1])
-    private_garden = st.select_slider("private garden", [0,1])
-    swimming_pool = st.select_slider("swimming pool", [0,1])
-    surface = st.slider("surface (m2)",0,1000)
-    transport_access = st.select_slider("transport access", [0,1])
 
-    feature_dict = {'traveler': [traveler],'bathroom': [bathroom], 'free_parking_on_site': [free_parking_on_site],
-                    'free_street_parking': [free_street_parking], 'heating': [heating], 'seaview': [seaview], 'AC': [AC],
-                    'wifi': [wifi], 'accepted_animals': [accepted_animals], 'tv': [tv], 'microwave_oven': [microwave_oven], 
-                    'smoker': [smoker], "backyard":[backyard], "workspace":[workspace], 'private_garden': [private_garden],
-                    'swimming_pool': [swimming_pool], 'surface': [surface],'transport_access': [transport_access]}
+    with st.sidebar :
+        
+        # Date :
+        now = datetime.now()+ timedelta(hours=1)
+        time = now.strftime("%H:%M:%S")
+        st.write("Heure à Paris : ",time)
+        
+        # Features params :
+        traveler = st.slider("number of traveler", 1, 15), 
+        bathroom = st.slider("number of bathroom", 0, 10),
+        free_parking_on_site = st.select_slider("Free parking on site", [0,1]),
+        free_street_parking = st.select_slider("Free parking on street", [0,1]),
+        heating = st.select_slider("heating", [0,1]),
+        seaview = st.select_slider("seaview", [0,1]),
+        AC = st.select_slider("AC (climatisation)", [0,1]),
+        wifi = st.select_slider("wifi", [0,1]),
+        accepted_animals = st.select_slider("accepted animals", [0,1]),
+        tv = st.select_slider("tv", [0,1]),
+        microwave_oven = st.select_slider("microwave_oven", [0,1]),
+        smoker = st.select_slider("smoker", [0,1]),
+        backyard = st.select_slider("backyard", [0,1]),
+        workspace = st.select_slider("workspace", [0,1]),
+        private_garden = st.select_slider("private garden", [0,1]),
+        swimming_pool = st.select_slider("swimming pool", [0,1]),
+        surface = st.slider("surface (m2)",0,1000),
+        transport_access = st.select_slider("transport access", [0,1]), 
 
-    # Predict button :
-    if st.button('Predict the rental price') :
-        prediction = predict_location(feature_dict)
-    
-    st.success(prediction)
+        feature_dict = {'traveler': [traveler],'bathroom': [bathroom], 'free_parking_on_site': [free_parking_on_site],
+                        'free_street_parking': [free_street_parking], 'heating': [heating], 'seaview': [seaview], 'AC': [AC],
+                        'wifi': [wifi], 'accepted_animals': [accepted_animals], 'tv': [tv], 'microwave_oven': [microwave_oven], 
+                        'smoker': [smoker], "backyard":[backyard], "workspace":[workspace], 'private_garden': [private_garden],
+                        'swimming_pool': [swimming_pool], 'surface': [surface],'transport_access': [transport_access]}
+
+        # Predict button :
+        if st.button('Predict the rental price') :
+            prediction = predict_location(feature_dict)
+        
+        st.success(prediction)
 
 
 # __name__ :
