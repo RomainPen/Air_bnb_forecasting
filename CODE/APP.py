3662a8d5467b56335859c81e05121a15cfd13df1
RomainPen <romain.pen.pro16@gmail.com>
2023-07-10 21:00:28 +0200

    update app.py

--- CODE/APP.py
+++ CODE/APP.py
@@ -47,24 +47,24 @@ def main():
         st.write("Heure à Paris : ",time)
         
         # Features params :
-        traveler = st.slider("number of traveler", 1, 15), 
-        bathroom = st.slider("number of bathroom", 0, 10),
-        free_parking_on_site = st.select_slider("Free parking on site", [0,1]),
-        free_street_parking = st.select_slider("Free parking on street", [0,1]),
-        heating = st.select_slider("heating", [0,1]),
-        seaview = st.select_slider("seaview", [0,1]),
-        AC = st.select_slider("AC (climatisation)", [0,1]),
-        wifi = st.select_slider("wifi", [0,1]),
-        accepted_animals = st.select_slider("accepted animals", [0,1]),
-        tv = st.select_slider("tv", [0,1]),
-        microwave_oven = st.select_slider("microwave_oven", [0,1]),
-        smoker = st.select_slider("smoker", [0,1]),
-        backyard = st.select_slider("backyard", [0,1]),
-        workspace = st.select_slider("workspace", [0,1]),
-        private_garden = st.select_slider("private garden", [0,1]),
-        swimming_pool = st.select_slider("swimming pool", [0,1]),
-        surface = st.slider("surface (m2)",0,1000),
-        transport_access = st.select_slider("transport access", [0,1]), 
+        traveler = st.slider("number of traveler", 1, 15)
+        bathroom = st.slider("number of bathroom", 0, 10)
+        free_parking_on_site = st.select_slider("Free parking on site", [0,1])
+        free_street_parking = st.select_slider("Free parking on street", [0,1])
+        heating = st.select_slider("heating", [0,1])
+        seaview = st.select_slider("seaview", [0,1])
+        AC = st.select_slider("AC (climatisation)", [0,1])
+        wifi = st.select_slider("wifi", [0,1])
+        accepted_animals = st.select_slider("accepted animals", [0,1])
+        tv = st.select_slider("tv", [0,1])
+        microwave_oven = st.select_slider("microwave_oven", [0,1])
+        smoker = st.select_slider("smoker", [0,1])
+        backyard = st.select_slider("backyard", [0,1])
+        workspace = st.select_slider("workspace", [0,1])
+        private_garden = st.select_slider("private garden", [0,1])
+        swimming_pool = st.select_slider("swimming pool", [0,1])
+        surface = st.slider("surface (m2)",0,1000)
+        transport_access = st.select_slider("transport access", [0,1])
 
     feature_dict = {'traveler': [float(traveler)],'bathroom': [float(bathroom)], 'free_parking_on_site': [float(free_parking_on_site)],
                     'free_street_parking': [float(free_street_parking)], 'heating': [float(heating)], 'seaview': [float(seaview)], 'AC': [float(AC)],
